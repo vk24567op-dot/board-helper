ef0ead73bab40f2e316397abce8f4e533181620b
VANSH RAJPUT <vk24567op@gmail.com>
2026-08-16 23:08:29 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -642,14 +642,12 @@ app.post("/api/practice", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-
 app.listen(
     PORT,
+    "0.0.0.0",
     () => {
-
         console.log(
             `Board Helper backend running on port ${PORT}`
         );
-
     }
 );
